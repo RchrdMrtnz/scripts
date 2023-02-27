a2b9ccb15b2b4e697252b64fa1af0d2cf4f647bb
RchrdPdrz <rchrd.pdrz.mrtnz@gmail.com>
2023-02-26 21:40:03 -0400

    time

--- python/speedtest/main.py
+++ python/speedtest/main.py
@@ -1,14 +1,14 @@
 import requests
 import time
 
-start=time.time()
 
 def timer():
+    start=time.time()
     response=requests.get('https://google.com')
     if response.status_code==200:
         end=time.time()
-        final=end-start
-        print(f'{final:.2f}')
+        final=(end-start)*100
+        print(f'{final:.2f}ms')
     else:
         print('404')
 
