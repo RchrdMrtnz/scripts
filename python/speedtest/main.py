1cd43a6ba296d84447a52f5b820855497ef0cb70
rchrdmrtnz <rchrd.pdrz.mrtnz@gmail.com>
2023-02-27 09:25:03 -0400

    try

--- python/speedtest/main.py
+++ python/speedtest/main.py
@@ -3,14 +3,18 @@ import time
 
 
 def timer():
-    start=time.time()
-    response=requests.get('https://8.8.8.8')
-    if response.status_code==200:
-        end=time.time()
-        final=(end-start)*100
-        print(f'{final:.2f}ms')
-    else:
-        print('404')
+    try:
+        start=time.time()
+        response=requests.get('https://google.com')
+        if response.status_code==200:
+           end=time.time()
+           final=(end-start)*100
+           print(f'{final:.2f}ms')
+        else:
+           print('404')
+
+    except:
+        print('500')
 
 
 if __name__ == "__main__":
